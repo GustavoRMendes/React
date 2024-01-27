4d73a721feb4a330e21bafd8cfc31b5531f62a4f
Gustavo-Mendess <gustavormendess@gmail.com>
2024-01-27 16:44:07 -0400

    Conhendo o JSX

---
+++ JSX/index.jsx
@@ -0,0 +1,12 @@
+const newElement = <h1>Olá, Mundo!</h1>
+const anotherElement = (
+   <div>
+    <h2>Subtitle</h2>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+)
