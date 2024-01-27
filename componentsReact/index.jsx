f06752ca548847a0905fa24247b8679ee8c1b422
Gustavo-Mendess <gustavormendess@gmail.com>
2024-01-27 17:08:50 -0400

    Componentes do React

---
+++ componentsReact/index.jsx
@@ -0,0 +1,17 @@
+// Componentes do React
+function MyLabel (){
+  return <label>Minha label</label>
+}
+
+function MyFirstButton(){
+  return <button>Meu botão</button>
+}
+
+function MyMain(){
+  return (
+    <div>
+      <h1>Título</h1>
+      <p>Descrição</p>
+    </div>
+  )
+}
