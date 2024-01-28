17e6fa787d720049acc80fd23e9d238c1d723ce1
Gustavo-Mendess <gustavormendess@gmail.com>
2024-01-28 17:38:42 -0400

    Vite com React

---
+++ firstProject/first-project-react/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    
+     <div>
+        <h1>Criando projeto com React</h1>
+        <h2>Utilizando Vite</h2>
+        <ul>
+          <li>Fazer exercícios</li>
+          <li>Ler documentação</li>
+          <li>Ser curioso</li>
+          <li>Foco e disciplina</li>
+        </ul>
+     </div>
+    
+  )
+}
+
