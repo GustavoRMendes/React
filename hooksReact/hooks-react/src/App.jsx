d48a50450707e10937aa6a38e612144def8b6944
Gustavo-Mendess <gustavormendess@gmail.com>
2024-01-31 14:36:37 -0400

    Hooks React

---
+++ hooksReact/hooks-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from './useCounter'
+export default function App(){
+  const counter = Counter()
+
+  return (
+    <>
+      <button
+      onClick={counter.handleClick}>
+      Count is {counter.count}
+      </button>
+    </>
+  )
+}
