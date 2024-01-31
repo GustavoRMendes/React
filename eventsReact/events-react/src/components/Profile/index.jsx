954d31b40a1ba26c60052e6ccfff43f41c9fdaba
Gustavo-Mendess <gustavormendess@gmail.com>
2024-01-31 10:17:16 -0400

    Usando o useState

--- eventsReact/events-react/src/components/Profile/index.jsx
+++ eventsReact/events-react/src/components/Profile/index.jsx
@@ -1,40 +1,38 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable react/prop-types */
-import styles from "./styles.module.css"
-import Title from "../Title"
-import ProfileSection from "./ProfileSection"
-import LinkButton from "../LinkButton"
-
-function handleClick(ev){
-  ev.preventDefault()
-  alert('Você seguiu este usuário!')
-}
+import styles from "./styles.module.css";
+import Title from "../Title";
+import ProfileSection from "./ProfileSection";
+import LinkButton from "../LinkButton";
+import { useState } from "react";
 export default function Profile(props) {
+  const [buttonFollow, setButtonFollow] = useState("Seguir");
+  function handleClick() {
+    alert("Você seguiu este usuário!");
+    setButtonFollow("Seguindo");
+  }
   return (
-        <div className={styles.container}>
-          <img className={styles.avatar} src={props.avatar} alt={props.name} />
-          <Title>
-          <span>{props.name}</span>
-          <button
-          className={styles.followButton}
-          onClick={handleClick}
-          >Follow
-          </button>
-          </Title>
-          <ProfileSection>{props.bio}</ProfileSection>
-          <ProfileSection>{props.phone}</ProfileSection>  
-          <ProfileSection>{props.email}</ProfileSection>
-          <ProfileSection
-          className={styles.links}
-          id='links-section'
-          data-test='some value'
-          aria-label='social links'
-          >
-          <LinkButton href={props.githubUrl}>Github</LinkButton>
-          <LinkButton href={props.linkedinUrl}>LinkedIN</LinkButton>
-          <LinkButton href={props.twitterUrl}>Twitter</LinkButton>
-          </ProfileSection>
-        </div>
-      )
+    <div className={styles.container}>
+      <img className={styles.avatar} src={props.avatar} alt={props.name} />
+      <Title>
+        <span>{props.name}</span>
+        <button className={styles.followButton} onClick={handleClick}>
+          {buttonFollow}
+        </button>
+      </Title>
+      <ProfileSection>{props.bio}</ProfileSection>
+      <ProfileSection>{props.phone}</ProfileSection>
+      <ProfileSection>{props.email}</ProfileSection>
+      <ProfileSection
+        className={styles.links}
+        id="links-section"
+        data-test="some value"
+        aria-label="social links"
+      >
+        <LinkButton href={props.githubUrl}>Github</LinkButton>
+        <LinkButton href={props.linkedinUrl}>LinkedIN</LinkButton>
+        <LinkButton href={props.twitterUrl}>Twitter</LinkButton>
+      </ProfileSection>
+    </div>
+  );
 }
-  
