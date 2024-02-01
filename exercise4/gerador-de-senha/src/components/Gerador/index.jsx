970d8cc6ce39d4efed1f71aea3003906f1baa367
Gustavo-Mendess <gustavormendess@gmail.com>
2024-02-01 09:18:56 -0400

    Atualizando arquivo

--- exercise4/gerador-de-senha/src/components/Gerador/index.jsx
+++ exercise4/gerador-de-senha/src/components/Gerador/index.jsx
@@ -1,28 +1,52 @@
-import Generator from "../../useGenerator";
 import style from "./style.module.css";
 import { useState } from "react";
 export default function Gerador() {
-  const generic = Generator();
-  const [copyText] = useState("Copiar");
+  const [copyText,setCopyText] = useState("Copiar");
+  const [password, setPassword] = useState("");
+  const [passwordSize,setPasswordSize] = useState(12)
 
-  function copyToClipboard() {
-    window.navigator.clipboard.writeText(generic.password);
+  function randomPassword() {
+    const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
+    let newPassword = ""
+    for (let i = 0; i < passwordSize; i++) {
+      const position = Math.floor(Math.random() * characters.length)
+      newPassword += characters[position]
+    }  
+    setPassword(newPassword)
+    setCopyText("Copiar")
   }
+  
+    function copyToClipboard() {
+    window.navigator.clipboard.writeText(password); 
+    setCopyText('Copiado')
+  }
+
+  const passwordRandom = () => setPassword(randomPassword);
+  
   return (
     <>
       <body>
         <h2 className={style.title}>Gerador de Senhas</h2>
+        <label className={style.label} htmlFor="passwordSize">Tamanho da senha: </label>
+        <input
+        className={style.input}
+        type="number"
+        id="passwordSize"
+        min={1}
+        value={passwordSize}
+        onChange={(evt) =>setPasswordSize( evt.target.value)}
+        />
         <button
           className={style.buttonGerator}
-          onClick={generic.generePassword}
+          onClick={passwordRandom}
         >
-          Gerar
+          Gerar senha de {passwordSize} caracteres.
         </button>
         <button className={style.buttonCopy} onClick={copyToClipboard}>
           {copyText}
         </button>
-        <div className={style.result}>{generic.password}</div>
+        <div className={style.result}>{password}</div>
       </body>
     </>
-  );
+  )
 }
