79d5fb8f8be6cad12fb2eef18beb6e4f8b669eb2
Gustavo-Mendess <gustavormendess@gmail.com>
2024-02-05 16:37:09 -0400

    Não usar o useEffect tanto

--- httpUseEffect/http-effect/src/App.jsx
+++ httpUseEffect/http-effect/src/App.jsx
@@ -1,23 +1,28 @@
-import { useEffect, useState } from "react"
-
+/* eslint-disable no-unused-vars */
+import {  useState } from "react"
 async function fetchPokemon() {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon/")
   const data = await response.json()
   return data.results
 }
-
 export default function App() {
   const [pokemon, setPokemon] = useState([])
-
-  useEffect(() => {
-    fetchPokemon().then(result => {
-      console.log("Requisição realizada")
-      console.log(result)
-      setPokemon(result)
-    })
-  }, [])
-
+  const [pokemonShown,setPokemonShown] = useState(null)
+  // As vezes não precisamos do useEffect()
+  if(pokemon.length === 0 ){
+      fetchPokemon().then(result => {
+        console.log("Requisição realizada")
+        console.log(result)
+        setPokemon(result)
+      })
+    }
   
+    const showDetails = async (url) => {
+      const data = await fetch(url).then(res => res.json())
+      console.log('Pokémon encontrado')
+      console.log(data)
+      setPokemonShown(data)
+    }
   return (
     <div className="app">
       <div>
@@ -26,7 +31,7 @@ export default function App() {
           {pokemon.map(mon => (
             <li key={mon.name}>
               <span>{mon.name}</span>
-              <button>
+              <button onClick={() => showDetails(mon.url)}>
                 Ver detalhes
               </button>
             </li>
