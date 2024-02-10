f4ab3ad9f1780ee131fa093a72d4596a598b5e42
Gustavo-Mendess <gustavormendess@gmail.com>
2024-02-09 20:11:05 -0400

    shadcn

--- shadcn/vite-project/src/App.jsx
+++ shadcn/vite-project/src/App.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
 
 export default function App() {
-  const [ count , setCount ] = useState(0);
-  function handleCount(){
-    setCount( count => count += 1);
+  const [count, setCount] = useState(0);
+  const [valueInput, setValueInput] = useState("");
+
+  function handleCount() {
+    setCount((count) => (count += 1));
   }
 
   return (
     <>
       <h1> useState() </h1>
       <div>
+        <label htmlFor="name">Nome: </label>
+        <input
+          type="text"
+          id="name"
+          onChange={(ev) => setValueInput(ev.target.value)}
+          placeholder="Digite seu nome"
+        />
+      </div>
+      <br />
+      <div>
         <button onClick={handleCount}>
-          Gustavo, você clicou no botão {count}! 
+          {valueInput}, você clicou no botão {count}!
         </button>
       </div>
     </>
-  )
+  );
 }
