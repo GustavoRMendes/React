9e08f675ce8fb974823da2afb8fc6fa6f8b1ce84
Gustavo-Mendess <gustavormendess@gmail.com>
2024-02-09 20:25:14 -0400

    modal

---
+++ modal/modal-react/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App(){
+  return (
+    <>
+    <div>
+      <h2>Modal</h2>
+      <button onClick={() => console.log("clicou")}>Abrir o modal</button>
+    </div>
+    
+    </>
+  )
+}
