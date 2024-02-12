8b7364d68d332cd335341062f9391dd5719649c3
Gustavo-Mendess <gustavormendess@gmail.com>
2024-02-12 16:56:58 -0400

    useReducer

--- useReducer/vite-project/src/App.jsx
+++ useReducer/vite-project/src/App.jsx
@@ -1,19 +1,34 @@
-import { useState } from "react";
+// useReducer
+// const [state,dispatch] = useReducer(reducer,initialArg,init?)
+// function reducer (state,action) 
+// swicth (action.type)
+// function click(){ dispatch({ type: 'action.type'})}
+
+import { useReducer } from "react";
 
 function App() {
-  const [count, setCount] = useState(0); 
-  const [text,setText] = useState(true)
-  function handleClick(){
-    setCount((count) => (count += 1))
-    setText(!text)
+  function reducer(state, action) {
+    switch (action.type) {
+      case "Increment":
+        return { ...state, count: state.count + 1 };
+      case "text":
+        return { ...state, text: !state.text };
+      default:
+        return "Nothing";
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    text: true,
+  });
+  function handleClick() {
+    dispatch({ type: "Increment" });
+    dispatch({ type: "text" });
   }
   return (
     <>
-    <button onClick={handleClick}>
-      This count is {count}
-    </button>
-    {text && <h2>useReducer</h2>}
-    
+      <button onClick={handleClick}>This count is {state.count}</button>
+      {state.text && <h2>useReducer</h2>}
     </>
   );
 }
